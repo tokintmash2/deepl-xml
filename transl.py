6487e4a3e1cf5a0a0a9a77be5b7e6034f115e8cf
Miikael Volkonski <miikael@volkonski.com>
2025-05-12 15:35:50 +0300

    Basic translation

--- transl.py
+++ transl.py
@@ -1,7 +1,22 @@
 import deepl
 
 auth_key = "a57ff645-14a2-4baf-9a76-9f2127927223:fx"  # Replace with your key
-deepl_client = deepl.DeepLClient(auth_key)
 
-result = deepl_client.translate_text("Hello, world!", target_lang="ET")
-print(result.text)  # "Bonjour, le monde !"
+translator = deepl.Translator(auth_key)
+
+text = "Suvaline kartul kasvas põllul ja muutus <i>roheliseks</i>, selle asemel, et olla sinine. Peremees pahandas."
+
+result = translator.translate_text(
+    text,
+    source_lang="ET",
+    target_lang="EN-GB",
+    tag_handling="xml",   # specify XML handling
+    ignore_tags=["i"]     # optionally, specify tags to ignore during translation
+)
+
+print(result.text)
+
+# deepl_client = deepl.DeepLClient(auth_key)
+
+# result = deepl_client.translate_text("Suvaline kartul kasvas põllul ja muutus <i>roheliseks</i>, selle asemel, et olla sinine. Peremees pahandas.", target_lang="EN-GB", source_lang="ET")
+# print(result.text)  # "Bonjour, le monde !"
