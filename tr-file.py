2bc687476930c2d73ed8c561b6a18030f898c569
Miikael Volkonski <miikael@volkonski.com>
2025-05-13 09:51:53 +0300

    hide key

--- tr-file.py
+++ tr-file.py
@@ -1,34 +1,46 @@
 import deepl
 import xml.etree.ElementTree as ET
 
-auth_key = "api_key"  
+f = open("key", "r")  
+auth_key = f.read().strip()
+f.close()
 translator = deepl.Translator(auth_key)
 
-# Define your namespace clearly
+ET.register_namespace('', 'http://docbook.org/ns/docbook')
+ET.register_namespace('xlink', 'http://www.w3.org/1999/xlink')
+ET.register_namespace('xi', 'http://www.w3.org/2001/XInclude')
+
 ns = {'db': 'http://docbook.org/ns/docbook'}
 
-# Register namespace to preserve prefixes
-ET.register_namespace('', 'http://docbook.org/ns/docbook')
-
-# Parse input XML file
 tree = ET.parse("source/input.xml")
 root = tree.getroot()
 
-# List of tags you want translated
-tags_to_translate = ['title', 'para', 'caption', 'tag', 'sect2']
+tags_to_translate = ['title', 'para', 'caption', 'medialabel', 'emphasis']
 
-# Translate relevant elements
-for elem in root.findall(".//db:*", ns):
-    tag_no_ns = elem.tag.split('}')[1]  # Extract local tag name without namespace
-    if tag_no_ns in tags_to_translate and elem.text and elem.text.strip():
-        # Call DeepL to translate text
+def translate_text_if_needed(text):
+    if text and text.strip():
         result = translator.translate_text(
-            elem.text.strip(),
-            source_lang="EN",  # Your original text seems English
-            target_lang="ET",
+            text.strip(),
+            source_lang="EN",
+            target_lang="FI",
             tag_handling="xml"
         )
-        elem.text = result.text
+        return result.text
+    return text
 
-# Write translated XML to output file
+for elem in root.findall(".//db:*", ns):
+    tag_no_ns = elem.tag.split('}')[1]
+
+    if tag_no_ns in tags_to_translate:
+        # Translate text before children
+        elem.text = translate_text_if_needed(elem.text)
+
+        # Translate tails of child elements
+        for child in elem.iter():
+            child.tail = translate_text_if_needed(child.tail)
+            
+root.set("xmlns:xlink", "http://www.w3.org/1999/xlink")
+root.set("xmlns:xi", "http://www.w3.org/2001/XInclude")
+
+# Save translated file
 tree.write("translated.xml", encoding="UTF-8", xml_declaration=True)
